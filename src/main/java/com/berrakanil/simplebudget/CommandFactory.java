d93e73ca6a5ef511eee1e7985fbee1d3fd7c249b
berrakanil <berrakanil@users.noreply.github.com>
2015-09-22 15:05:03 +0100

    Update CommandFactory.java

--- src/main/java/com/berrakanil/simplebudget/CommandFactory.java
+++ src/main/java/com/berrakanil/simplebudget/CommandFactory.java
@@ -6,10 +6,7 @@ public class CommandFactory {
 
 	public static Command createCommand(String commandName, String[] commandParameters, Account account)
 			throws Exception {
-
-		//Show usage by default
 		Command command = new UsageCommand(commandParameters, account);
-
 		if (compareCommand(AddCommand.class, commandName)) {
 			command = new AddCommand(commandParameters, account);
 		} else if (compareCommand(ViewCommand.class, commandName)) {
